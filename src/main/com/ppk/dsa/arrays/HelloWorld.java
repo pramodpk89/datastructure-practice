1cb3608c0c83e19d9a495e3b06be9784d11b7f1d
pramodpk89 <Pr@mod1989>
2021-03-16 12:12:23 +0530

    first commit

--- src/main/com/ppk/dsa/arrays/HelloWorld.java
+++ src/main/com/ppk/dsa/arrays/HelloWorld.java
@@ -2,6 +2,6 @@ package com.ppk.dsa.arrays;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hellon world");
+        System.out.println("Hello world");
     }
 }
