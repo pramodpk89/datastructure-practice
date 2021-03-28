99b25816b48a36b3fc7ebecc0ef9f32e834e99db
pramodpk89 <Pr@mod1989>
2021-03-28 16:58:57 +0530

    Armstrong number

---
+++ src/main/com/ppk/dsa/arrays/ArmstrongNumber.java
@@ -0,0 +1,20 @@
+package com.ppk.dsa.arrays;
+
+/**
+ * A number is an armstrong number if sum of cubes of its digit is equal to the number
+ */
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        System.out.println(armstrongNumCheck("153"));
+    }
+
+    private static boolean armstrongNumCheck(String str) {
+        String[] chars = str.split("");
+        int runningSum = 0;
+        for (String i : chars) {
+            int val = Integer.valueOf(i);
+            runningSum = runningSum + (val * val * val);
+        }
+        return (runningSum == Integer.valueOf(str));
+    }
+}
